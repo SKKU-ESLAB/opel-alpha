a7386d1f16a434e0f37e8ba070fc650cd4a36ab3
InJung Hwang <sinban04@users.noreply.github.com>
2016-08-25 17:28:35 +0900

    Raspberry pi sensor manager (#49)
    * make the prototype of shared object sensor manager
    
    * Make build script for sensor manager with shared objects
    
    * Make the build script and version of sensor manager with shared object
    
    * test complete: sensor manager with shared object
    
    * Solve merge conflict in the previous commit, and modify node/src 'sensfw' 'sysfw' build directory bug
    
    Arrange the file to neat state
    
    Set test environment
    
    Make .gitignore clean
    
    * build.sh sensor8_test.c removed
    
    Arrange Files : leave only source file
    
    * Remove sensor list configuration dependency from NIL

--- node/src/sensfw/test2.js
+++ node/src/sensfw/test2.js
@@ -2,13 +2,6 @@ console.log('Code start!');
 
 
 var sensorManager = require("./build/Release/nil");
-var fs = require('fs');
-
-
 
 console.log('the value is ' + sensorManager.Get("LIGHT").LIGHT);
-
-
-
-
 
