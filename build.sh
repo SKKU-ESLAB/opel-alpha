71a9384fbd69bbd7c9ac0134e792cea6f5be0f38
Hyemin Lee <gudbooy@gmail.com>
2016-08-29 03:00:14 +0000

    delete the files remained in nvidia-tx1-working branch

--- build.sh
+++
@@ -1,7 +0,0 @@
-#g++ abtGstEncoder.c $(pkg-config --cflags --libs gstreamer-1.0) -lpthread -lgstapp-1.0 -I/home/ubuntu/workspace/2-a-brighter-tomorrow
-
-
-g++ -c abtMisc.cpp -o abtMisc.o -lptread
-g++ -c abtGstEncoder.cpp -o abtGstEncoder.o $(pkg-config --cflags --libs gstreamer-1.0) -lpthread -lgstapp-1.0
-g++ -c main.c -o main.o $(pkg-config --cflags --libs gstreamer-1.0) -lpthread -lgstapp-1.0
-g++ main.o abtMisc.o abtGstEncoder.o -o main $(pkg-config --cflags --libs gstreamer-1.0) -lpthread -lgstapp-1.0
