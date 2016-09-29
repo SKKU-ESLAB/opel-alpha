0ce8dced62cd428c81d3296dc4e89780531be991
Injung <sinban04@gmail.com>
2016-09-29 12:25:33 +0000

    build succeed before debugging runtime

--- node/src/sensfw/test3.js
+++ node/src/sensfw/test3.js
@@ -1,2 +1,12 @@
+console.log('Code start!');
 
-var wpi = require('Wiringpi');
+var sensorManager = require("./build/Release/nil");
+
+sensorManager.On("SOUND", "PERIODIC", 1000, function(sound){
+		console.log("callback came");
+		if(sound.SOUND > 2){
+				console.log('SOUND event!\n');
+		}
+		console.log("data: " + sound);
+});
+
