3f0ae3f33eaddc856e90870b8b73ecaa4f1e333a
Hyemin Lee <gudbooy@gmail.com>
2016-10-07 13:12:02 +0000

    Face detction's path is modified.

--- node/src/camfw/facedetection/src/OPELFaceDetection.cc
+++ node/src/camfw/facedetection/src/OPELFaceDetection.cc
@@ -16,7 +16,7 @@
 using namespace std;
 using namespace cv;
 
-string face_cascade = "/home/ubuntu/opel-alpha/node/facedetection/haarcascade_frontalface_alt.xml";
+string face_cascade = "/home/ubuntu/haarcascade_frontalface_alt.xml";
 
 class OPELFaceDetection : public Nan::AsyncWorker
 {
