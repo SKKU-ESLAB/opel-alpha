cc66893cef7617152ebed349d2869f742e1a096c
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-01-13 18:47:05 +0000

    Default applications are added

---
+++ sample-apps/GoodMorning/index.js
@@ -0,0 +1,75 @@
+var opel_dir = process.env.OPEL_DIR;
+
+var sysAppNIL = require(opel_dir + '/node/sysfw/nil');
+var cameraNIL = require(opel_dir + '/node/OPELFaceDetection');
+require('date-utils');
+
+
+var count = 6;
+var name, time;
+var configPage = sysAppNIL.makeConfigPage();
+configPage = sysAppNIL.addStrTextbox(configPage, "Name", "What is your name?", 10);
+
+configPage = sysAppNIL.addTimeDialog(configPage, "Time", "What time do you want to wake up?", 1);
+
+console.log(configPage);
+
+
+sysAppNIL.sendConfigPage(configPage, function(configurableData){
+
+
+	name = sysAppNIL.getData(configurableData, "Name");
+	console.log("<GoodMorning>Set name : "+ name);
+
+	time = sysAppNIL.getData(configurableData, "Time");
+	console.log("<GoodMorning>Set time : "+ time);
+
+	var repeat = setInterval(function(){
+		var dt = new Date();
+		var d = dt.toFormat('HH24:MI');
+
+		if (time == d){
+			console.log('<GoodMorning> : It`s time to wake up'); 
+		
+				var repeat3 = setInterval(function(){	
+				cameraNIL.faceDetection(count, function(err, numberOfFace){
+			
+
+  		  			if(numberOfFace > 0){
+						console.log('Face Detection !! ');
+						console.log('Number of Face : ' + numberOfFace +'\n');
+				        process.exit(1);
+					}
+
+			        else{
+        				console.log('<GoodMorning> Show me your handsome face !!! \n');
+
+			            var notiPage = sysAppNIL.makeEventPage("Time to wake up");
+						notiPage = sysAppNIL.addEventText(notiPage, "Hey " + name);
+						notiPage = sysAppNIL.addEventText(notiPage, "Wake up now");
+						notiPage = sysAppNIL.addEventImg(notiPage, "/home/pi/morning.jpg");
+						sysAppNIL.sendEventPageWithNoti(notiPage);
+
+			        }
+      			});
+
+				}, 10000);
+			
+	
+		}
+		else{
+			console.log('<GoodMorning> Waiting for the time [' + time + '] /// cur time [' + d +']');
+		}
+
+	}, 20000); //20sec
+
+});
+
+sysAppNIL.onTermination(function(){
+
+		console.log('<onTermination> Called');
+});
+
+
+var repeat2 = setInterval(function(){
+		}, 50000);
---
+++ sample-apps/SmartBB/index.js
@@ -0,0 +1,39 @@
+var opel_dir = process.env.OPEL_DIR;
+
+var OPELCameraAddon = require(opel_dir +'/node/OPELRecording');
+var faceDetectAddon = require(opel_dir +'/node/OPELFaceDetection');
+var sensorAddon = require(opel_dir +'/node/sensfw/nil');
+var systemManager = require(opel_dir +'/node/sysfw/nil');
+
+
+var recObj = new OPELCameraAddon.OPELRecording();
+var count = 10000;
+var dt, d;
+var cnt=0;
+
+var path = '/home/ubuntu/opelData/';
+var alreadyRunning = false;
+var temp;
+
+//var repeat = setInterval(function(){
+		alreadyRunning = true;
+		faceDetectAddon.faceDetection(count, function(err, numberOfFace){
+				if(numberOfFace > 0)
+				{
+				
+				console.log("FaceDetection");
+				recObj.RecordingStart(path+'BB[12345].mp4', 10, function(err){
+						var notiPage = systemManager.makeEventPage('Blackbox video was recorded');
+						notiPage = systemManager.addEventText(notiPage, 'File name : '+ d + '.mp4');
+						systemManager.sendEventPageWithNoti(notiPage);
+						});
+				}
+				else
+				alreadyRunning = false; 
+				});
+//}, 500);
+
+/*systemManager.onTermination(function(){
+
+		console.log('<onTermination> Called');
+		});*/
