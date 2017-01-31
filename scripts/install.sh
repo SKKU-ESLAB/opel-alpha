314b0337d6276cb056cc8950cf4a3ef881af41fe
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-01-31 04:42:06 +0000

    Deprecate bin directory and add several options on install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -25,14 +25,18 @@
 show_usage() {
   echo \
 "$0: OPEL install script
-usage: $0 [-h] [--help]
+usage: $0 --target=<TARGET_BOARD> [-h] [--help]
+
+Available target boards(--target):
+  tegraTX1
 
 You can pass following environment variables to this script.
  - OPEL_OUT_DIR: OPEL build output directory
  - OPEL_BIN_DIR: OPEL binary directory
  - OPEL_CONFIG_DIR: OPEL config directory
  - OPEL_SENSOR_DRIVER_DIR: OPEL sensor driver directory
- - OPEL_APPS_DIR: OPEL application directory"
+ - OPEL_APPS_DIR: OPEL application directory
+ - OPEL_DATA_DIR: OPEL data directory"
 }
 
 # Get help
@@ -54,6 +58,10 @@ eval set -- "$PARSED"
 ARG_HELP=n
 while true; do
   case "$1" in
+    -t|--target)
+      ARG_TARGET_BOARD="$2"
+      shift 2
+      ;;
     -h|--help)
       ARG_HELP=y
       shift
@@ -63,7 +71,7 @@ while true; do
       break
       ;;
     *)
-      echo "Programming error"
+      echo "Invalid arguments"
       exit 3
       ;;
   esac
@@ -74,6 +82,20 @@ if [ $ARG_HELP = "y" ]
 then
   show_usage
   exit 0
+fi
+
+# Check target board
+if [ $ARG_TARGET_BOARD = "tegraTX1" ]
+then
+  echo "Target board $ARG_TARGET_BOARD is selected."
+  TARGET_DIR=target/$ARG_TARGET_BOARD
+  if [ -e $TARGET_DIR ]
+  then
+    echo "Target board directory does not exist."
+  fi
+else
+  echo "Invalid target board: $ARG_TARGET_BOARD"
+  exit 3
 fi
 
 # Determine environment variables
@@ -98,6 +120,26 @@ determine_var() {
       if [ ! -e ${!ENV_NAME} ]
       then
         echo "${!ENV_NAME} does not exist."
+
+        if [ $IS_DIRECTORY = y ]
+        then
+          IS_CREATE_NEW=y
+          
+          while [ : ];
+          do
+            echo -n "Do you want to create a new directory? (y/n; default=y): "
+            read IS_CREATE_NEW
+            if [ $IS_CREATE_NEW = y ] || [ $IS_CREATE_NEW = n];
+            then
+              LOOP_CONTINUE=n
+              break;
+            fi
+          done
+          if [ $IS_CREATE_NEW = y ]
+          then
+            mkdir -p ${!ENV_NAME}
+          fi
+        fi
       else
         if [ $IS_DIRECTORY = y ]
         then
@@ -120,7 +162,9 @@ determine_var() {
   done
 
   echo "$ENV_NAME= ${!ENV_NAME}"
+  echo ""
 }
+echo "* OPEL Environment variable setting"
 # 1: OPEL_OUT_DIR
 determine_var "OPEL_OUT_DIR" "OPEL output directory" y
 # 2: OPEL_BIN_DIR
@@ -131,6 +175,20 @@ determine_var "OPEL_CONFIG_DIR" "OPEL config directory" y
 determine_var "OPEL_SENSOR_DRIVER_DIR" "OPEL sensor driver directory" y
 # 5: OPEL_APPS_DIR
 determine_var "OPEL_APPS_DIR" "OPEL sensor driver directory" y
+# 6: OPEL_DATA_DIR
+determine_var "OPEL_DATA_DIR" "OPEL data directory" y
+# 7: THIS_SCRIPT_DIR
+THIS_SCRIPT_DIR=$(dirname "$0")
 
-# Install OPEL
+# TODO: Check OPEL_OUT_DIR
+check_out_dir() {
+}
+check_out_dir
 
+# Install OPEL_BIN_DIR
+cp -R ${OPEL_OUT_DIR}/bin ${OPEL_BIN_DIR}
+cp ${THIS_SCRIPT_DIR}/opel.py ${OPEL_BIN_DIR}
+cp ${THIS_SCRIPT_DIR}/run_opel ${OPEL_BIN_DIR}
+cp ${THIS_SCRIPT_DIR}/opel_p2p_setup.sh ${OPEL_BIN_DIR}
+
+# Install OPEL_CONFIG_DIR
