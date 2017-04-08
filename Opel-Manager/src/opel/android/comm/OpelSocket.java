445b549c0469d909532b15e98a11c4638b304aa1
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-04-08 20:43:31 +0900

    Fix bluetooth communication failure in SensorView

--- Opel-Manager/src/opel/android/comm/OpelSocket.java
+++ Opel-Manager/src/opel/android/comm/OpelSocket.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.lang.reflect.Method;
 import java.util.Set;
 import java.util.UUID;
 
@@ -159,48 +160,16 @@ public class OpelSocket {
 		case CONN_TYPE_BT:
 			UUID uuid = OpelUtil.name2uuid(intf_name);
 			Log.d(OpelUtil.TAG, intf_name+uuid.toString());
-			BluetoothSocket tmpSock = null;
+			BluetoothSocketWrapper tmpSock = null;
 			Set<BluetoothDevice> pairedDevices = BluetoothAdapter.getDefaultAdapter().getBondedDevices();
-			
+
 			if(pairedDevices.size() > 0){
 				for(BluetoothDevice tmpDevice : pairedDevices){
-					try {
-						Log.d(OpelUtil.TAG, "Connecting to : " + tmpDevice.getAddress());
-						tmpSock = tmpDevice.createInsecureRfcommSocketToServiceRecord(uuid);
-						if(tmpSock != null){
-							stat = STAT_CONNECTING;
-							tmpSock.connect();
-						}
-						if(tmpSock != null && tmpSock.isConnected()){
-							Log.d(OpelUtil.TAG, "Connected to : " + tmpDevice.getAddress());
-							bt_client_sock = tmpSock;
-							InputStream tmpIn = null;
-							OutputStream tmpOut = null;
-							try{
-								tmpIn = bt_client_sock.getInputStream();
-								tmpOut = bt_client_sock.getOutputStream();
-							} catch (IOException e){
-								Log.e(OpelUtil.TAG, "Sockets not created", e);
-							}
-							
-							mmInStream = tmpIn;
-							mmOutStream = tmpOut;		
-							stat = STAT_CONNECTED;
-							res = OpelSCModel.COMM_ERR_NONE;
-							break;
-						}
-						else{
-							tmpSock = null;
-							Log.d(OpelUtil.TAG, "No service in " +tmpDevice.getAddress());
-						}
-					} catch (IOException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
+
 				}
 				Log.d(OpelUtil.TAG, "Connected or Disconnected");
 			}
-			
+
 			break;
 		case CONN_TYPE_WD:
 		case CONN_TYPE_WF:
@@ -219,5 +188,113 @@ public class OpelSocket {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	// RedCarrottt: Fix Bluetooth connection failure bug (Issue #103, #112)
+	public static class NativeBluetoothSocket implements
+			BluetoothSocketWrapper {
+		private BluetoothSocket socket;
+
+		public NativeBluetoothSocket(BluetoothSocket tmp) {
+			this.socket = tmp;
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException {
+			return socket.getInputStream();
+		}
+
+		@Override
+		public OutputStream getOutputStream() throws IOException {
+			return socket.getOutputStream();
+		}
+
+		@Override
+		public String getRemoteDeviceName() {
+			return socket.getRemoteDevice().getName();
+		}
+
+		@Override
+		public void connect() throws IOException {
+			socket.connect();
+		}
+
+		@Override
+		public String getRemoteDeviceAddress() {
+			return socket.getRemoteDevice().getAddress();
+		}
+
+		@Override
+		public void close() throws IOException {
+			socket.close();
+		}
+
+		@Override
+		public BluetoothSocket getUnderlyingSocket() {
+			return socket;
+		}
+	}
+
+	public class FallbackBluetoothSocket extends NativeBluetoothSocket {
+		private BluetoothSocket fallbackSocket;
+
+		public FallbackBluetoothSocket(BluetoothSocket tmp) throws
+				FallbackException {
+			super(tmp);
+			try {
+				Class<?> clazz = tmp.getRemoteDevice().getClass();
+				Class<?>[] paramTypes = new Class<?>[]{Integer.TYPE};
+				Method m = clazz.getMethod("createRfcommSocket", paramTypes);
+				Object[] params = new Object[]{Integer.valueOf(1)};
+				fallbackSocket = (BluetoothSocket) m.invoke(tmp
+						.getRemoteDevice(), params);
+			} catch (Exception e) {
+				throw new FallbackException(e);
+			}
+		}
+
+		@Override
+		public InputStream getInputStream() throws IOException {
+			return fallbackSocket.getInputStream();
+		}
+
+		@Override
+		public OutputStream getOutputStream() throws IOException {
+			return fallbackSocket.getOutputStream();
+		}
+
+		@Override
+		public void connect() throws IOException {
+			fallbackSocket.connect();
+		}
+
+		@Override
+		public void close() throws IOException {
+			fallbackSocket.close();
+		}
+	}
+
+	public static class FallbackException extends Exception {
+		private static final long serialVersionUID = 1L;
+
+		public FallbackException(Exception e) {
+			super(e);
+		}
+	}
+
+	public static interface BluetoothSocketWrapper {
+		InputStream getInputStream() throws IOException;
+
+		OutputStream getOutputStream() throws IOException;
+
+		String getRemoteDeviceName();
+
+		String getRemoteDeviceAddress();
+
+		BluetoothSocket getUnderlyingSocket();
+
+		void connect() throws IOException;
+
+		void close() throws IOException;
 	}
 }
