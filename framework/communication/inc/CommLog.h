536027ab1be27665788ed777b68022e6ee75aa14
Gyeonghwan Hong <redcarrottt@gmail.com>
2017-08-14 11:35:19 +0000

    Update license

--- framework/communication/inc/CommLog.h
+++ framework/communication/inc/CommLog.h
@@ -1,28 +1,20 @@
-/*
- * OPEL Communication Framework
- * 
+/* Copyright (c) 2015-2017 CISS, and contributors. All rights reserved.
+ *
+ * Contributor: Eunsoo Park<esevan.park@gmail.com>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ *  http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *
  */
 
-/**
- * This file declares cmfw log functions and data structure.
- *
- * @file		CommLog.h
- * @author	Eunsoo Park (esevan.park@gmail.com)
- * @version	0.2
- * @Last modified 2016-5-19
- */
 #ifndef __CMFW_LOG_H__
 #define __CMFW_LOG_H__
 
